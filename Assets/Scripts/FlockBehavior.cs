fc5c59d495ee72943443027a8c3d0a2706e3f285
Jflinchum <jonflinchum@gmail.com>
2015-08-19 21:35:59 -0400

    - Added going around untagged objects - Created a waypoint prefab - Created waypoint script

--- Assets/Scripts/FlockBehavior.cs
+++ Assets/Scripts/FlockBehavior.cs
@@ -20,16 +20,20 @@ public class FlockBehavior : MonoBehaviour {
 
 	private Rigidbody rbody;
 	private bool drop = false;
+	private bool avoiding = false;
 	// Use this for initialization
 	void Start () {
 		rbody = transform.GetComponent<Rigidbody> ();
 
-		rbody.AddForce(new Vector3(Random.Range (-3, 3), Random.Range (-3,3), maxSpeed));
+		rbody.AddForce(new Vector3(Random.Range (-100, 100), Random.Range (-100,100), maxSpeed*10));
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		if (!drop) {
+			//Reset avoiding check
+			avoiding = false;
+
 			//Speed limiter
 			if (rbody.velocity.magnitude >= maxSpeed) {
 				rbody.velocity = rbody.velocity.normalized * maxSpeed;
@@ -39,16 +43,19 @@ public class FlockBehavior : MonoBehaviour {
 			if (rbody.velocity.magnitude != 0)
 				transform.rotation = Quaternion.LookRotation (rbody.velocity);
 
+			//All of the objects found within the sight range
 			Collider[] neighbors = Physics.OverlapSphere (transform.position, sightRad);
 
+			//For alignment
 			int alignNeighbors = 0;
 			Vector3 alignment = new Vector3 (0, 0, 0);
 
+			//For cohesion
 			int cohNeighbors = 0;
 			Vector3 cohesionPos = new Vector3( 0, 0, 0);
 
 			for (int i = 0; i < neighbors.Length; i++) {
-				if (neighbors [i].tag == "Flock") {
+				if (neighbors [i].tag == "Flock" && avoiding == false) {
 					//The vector and distance to the object
 					Vector3 flockVector = neighbors [i].transform.position - transform.position;
 					float flockDist = Vector3.Distance (neighbors [i].transform.position, transform.position);
@@ -73,12 +80,21 @@ public class FlockBehavior : MonoBehaviour {
 					float objDist = Vector3.Distance (neighbors [i].transform.position, transform.position);
 					if (objDist <= avoidDist)
 						rbody.AddForce (avoidDirect.normalized * avoidForce);
+					avoiding = true;
 				}
-			//For other objects to go around
-			else {
-					Vector3 closestPos = neighbors[i].bounds.ClosestPoint(transform.position);
-					Vector3 toCenter = neighbors[i].transform.position - transform.position;
-
+				//For other objects to go around
+				else {
+					if(Physics.CapsuleCast(transform.position, transform.position + (transform.forward * avoidDist), GetComponent<SphereCollider>().radius + (GetComponent<SphereCollider>().radius/2), transform.forward)){
+						Vector3 closestPos = neighbors[i].bounds.ClosestPoint(transform.position);
+						Vector3 toCenter = neighbors[i].transform.position - transform.position;
+						Vector3 direction = toCenter - closestPos;
+						if(Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+							direction = new Vector3(-direction.x - (Mathf.Sign (direction.x) * neighbors[i].bounds.size.x) , direction.y, direction.z).normalized * avoidForce;
+						else
+							direction = new Vector3(direction.x, -direction.y - ( Mathf.Sign (direction.y) * neighbors[i].bounds.size.y), direction.z).normalized * avoidForce;
+						avoiding = true;
+						rbody.AddForce(direction);
+					}
 				}
 			}
 			//Alignment force
@@ -95,6 +111,7 @@ public class FlockBehavior : MonoBehaviour {
 		if (collision.transform.tag != "Flock") {
 			drop = true;
 			GetComponent<TrailRenderer>().enabled = false;
+			GetComponent<Rigidbody>().useGravity = true;
 		}
 	}
 }
---
+++ Assets/Scripts/WayPointManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WayPointManager : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown) {
+
+		}
+	}
+}
